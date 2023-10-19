6da4975d94a3daae7d2cc28c5dccad093a556b93
CodinjaoftheWorld <gauravsaini.728@gmail.com>
2023-10-19 21:49:44 +0530

    formating the file

--- create_index.py
+++ create_index.py
@@ -2,18 +2,16 @@ import pandas as pd
 import openai
 import os
 from dotenv import load_dotenv, find_dotenv
-# from openai.embeddings_utils import get_embedding
 import numpy as np
 import pickle
 import faiss
 
 
-
+# Set the OPENAI API Key as the environment variable
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.getenv('OPENAI_API_KEY')
-# print(openai.api_key)
 
-# CONFIGS
+# Config Paths
 INPUT_FILE_NAME = "insurance.csv"
 EMBEDDING_FILE_NAME = "embedding_array.pickle"
 INPUT_FILE_DIR = "input"
@@ -23,15 +21,15 @@ OUTPUT_INDEX_FILE_NAME = "index.pickle"
 
 
 def create_text(row):
+    # Process the text before sending it to LLM
     return "question- " +str(row['question'])+ "\n" + "answer- "+str(row['answer'])
 
 def generate_embedding_array(embeddings, EMBEDDING_FILE_NAME, OUTPUT_FILE_DIR):
+    # Generate embeddings in numpy array and saving it in a pickle format
     all_embeddings = []
     for i in embeddings:
         all_embeddings.append(i['embedding'])
-    
     embedding_array = np.array(all_embeddings)
-    
 
     with open(f"{OUTPUT_FILE_DIR}/{EMBEDDING_FILE_NAME}", 'wb') as pickle_file:
         pickle.dump(embedding_array, pickle_file)
@@ -44,7 +42,7 @@ def create_faiss_index(embeddings_path, OUTPUT_INDEX_FILE_NAME, OUTPUT_FILE_DIR)
     os.path.abspath(__file__)
     ) 
 
-    # Defining the dimensions for openai embedding model which is 1536 os.path.join(ROOT_DIR, INPUT_FILE_DIR, EMBEDDING_FILE_NAME)
+    # Defining the dimensions for openai embedding model which is 1536
     d = 1536
     # Index creation
     index = faiss.IndexFlatIP(d)
@@ -53,7 +51,6 @@ def create_faiss_index(embeddings_path, OUTPUT_INDEX_FILE_NAME, OUTPUT_FILE_DIR)
     with open(embeddings_path, 'rb') as f:
         embeddings_content = f.read()
     embeddings = pickle.loads(embeddings_content)
-
     embeddings = embeddings.astype(np.float32)
 
     # Adding embedding to the index
@@ -66,24 +63,25 @@ def create_faiss_index(embeddings_path, OUTPUT_INDEX_FILE_NAME, OUTPUT_FILE_DIR)
     return
 
 def convert_masterdata_to_pickle(df, OUTPUT_MASTERDATA_FILE_NAME, OUTPUT_FILE_DIR):
-    # create the root diretory path
+    # Create the root diretory path
     ROOT_DIR = os.path.dirname(
     os.path.abspath(__file__)
     )
+    # Reading the master data and converting it into pickle file
     df.to_pickle(os.path.join(ROOT_DIR, OUTPUT_FILE_DIR, OUTPUT_MASTERDATA_FILE_NAME))
     return
 
 def main():
-    # create the root diretory path
+    # Create the root diretory path
     ROOT_DIR = os.path.dirname(
     os.path.abspath(__file__)
     )
-    # create the input file path
+    # Create the input file path
     input_file_path = os.path.join(ROOT_DIR, "input", INPUT_FILE_NAME)
 
     # Read the masterdata csv file
     df = pd.read_csv(input_file_path)
-    # create the input for mebedding creation
+    # Create the input for mebedding creation
     df['text'] = df.apply(lambda row: create_text(row), axis=1)
 
     # Genertae embeddings for all the rows
